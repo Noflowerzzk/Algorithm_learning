0884406e1bffd831aaaa99fb54d6cafd6a6b719c
Noflowerzzk <Noflowerzzk@outlook.com>
2024-08-11 16:20:38 +0800

    The first commit

---
+++ Prefix_Sum_and_Difference_Array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 确定n大小不用difine
+const int N = 1e5 + 3; // 加一定的值，方便
+
+// 开全局数组自动初始化
+int a[N];
+
+int main()
+{
+    // 基操：取消同步流
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+
+    cout << "Hello world!" << '\n'; // 不用endl
+    return 0;
+}
